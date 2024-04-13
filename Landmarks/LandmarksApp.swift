5c353a4a9083f17dfc4815b02a87c102453861fb
Agrima KP <qvistn111@gmail.com>
2024-04-13 12:04:37 +1000

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Agrima Purakkal on 13/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
